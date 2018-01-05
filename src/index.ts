fdba1f9f431375ffeb146ab4c75eb54ade3d5e44
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-01-05 10:28:11 -0800

    add more custom colors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as supports from 'supports-color'
 import chalk from 'chalk'
 import * as ansiStyles from 'ansi-styles'
+import { deprecate } from 'util'
+import * as stripAnsi from 'strip-ansi'
 
 export const CustomColors: {
   supports: typeof supports
@@ -11,8 +13,10 @@ export const CustomColors: {
   configVar: (s: string) => string
   release: (s: string) => string
   cmd: (s: string) => string
+  pipeline: (s: string) => string
   app: (s: string) => string
   heroku: (s: string) => string
+  stripColor: (s: string) => any
 } = {
   supports,
   // map gray -> dim because it's not solarized compatible
@@ -23,12 +27,17 @@ export const CustomColors: {
   configVar: chalk.green,
   release: chalk.blue.bold,
   cmd: chalk.cyan.bold,
+  pipeline: chalk.green.bold,
   app: (s: string) => color.heroku(`⬢ ${s}`),
   heroku: (s: string) => {
     if (!color.supports) return s
     let has256 = color.supports.has256 || (process.env.TERM || '').indexOf('256') !== -1
     return has256 ? '\u001b[38;5;104m' + s + ansiStyles.reset.open : chalk.magenta(s)
   },
+  stripColor: (s: string) =>
+    deprecate(() => {
+      return stripAnsi(s)
+    }, '.stripColor is deprecated. Please import the "strip-ansi" module directly instead.'),
 }
 
 export const color: typeof CustomColors & typeof chalk = new Proxy(chalk, {
